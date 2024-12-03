7422745d4b7affb278dbc8241852b3e557f96feb
SePt1k0404 <d.yra3004@gmail.com>
2024-12-03 22:32:24 +0200

    Start working with 14.2

---
+++ trello-tasks/task-14/project-2/sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "audio-guide-cache";
+const assetsToCache = [];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
